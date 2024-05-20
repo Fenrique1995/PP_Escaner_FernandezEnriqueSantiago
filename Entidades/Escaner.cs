b53a615ff6409065ee6a8ed3c486bb348625465c
unknown <fenrique310@gmail.com>
2024-05-20 08:47:22 -0300

    metodos get agregados a Escaner y tambien el constructor

--- Entidades/Escaner.cs
+++ Entidades/Escaner.cs
@@ -23,5 +23,21 @@ namespace Entidades
         private Departamento locacion;
         private string marca;
         private TipoDoc tipo;
+
+        public Escaner(string marca, TipoDoc tipo)
+        {
+            this.marca = marca;
+            this.tipo = tipo;
+        }
+
+        public List<Documento> ListaDocumentos { get{ return listaDocumentos; } }
+        public Departamento Locacion { get { return locacion;} }
+        public string Marca { get {  return marca; } }
+        public TipoDoc Tipo { get { return tipo; } }
+
+        public static bool CambiarEstadoDocumento (Documento d)
+        {
+            return true;
+        }
     }
 }
