94273b5887e39aa87936dc5913c013baf460507b
unknown <fenrique310@gmail.com>
2024-05-21 23:16:55 -0300

    Informes terminado... creo

--- Entidades/Informes.cs
+++ Entidades/Informes.cs
@@ -6,31 +6,64 @@ using System.Threading.Tasks;
 
 namespace Entidades
 {
-    static class Informes
+    public static class Informes
     {
-        public static void MostrarDistribuidos(Escaner e, out int extencion, out int cantidad, out string resumen)
+        public static void MostrarDistribuidos(Escaner e, out int extension, out int cantidad, out string resumen)
         {
-
+            MostrarDocumentosPorEstado(e, Paso.Distribuido, out extension, out cantidad, out resumen);
         }
 
-        private static void MostrarDocumentosPorEstado(Escaner e,Paso paso, out int extencion, out int cantidad, out string resumen)
+        private static void MostrarDocumentosPorEstado(Escaner e,Paso paso, out int extension, out int cantidad, out string resumen)
         {
+            extension = 0;
+            cantidad = 0;
+            resumen = "";
+            switch (paso)
+            {
+                case Paso.Inicio:
+                    extension = 1;
+                    break;
+                case Paso.Distribuido:
+                    extension = 2;
+                    break;
+                case Paso.EnEscaner:
+                    extension = 3;
+                    break;
+                case Paso.EnRevision:
+                    extension = 4;
+                    break;
+                case Paso.Terminado:
+                    extension = 5;
+                    break;
+            }
+            if (e.Tipo == TipoDoc.libro || e.Tipo == TipoDoc.mapa)
+            {
+                foreach (var item in e.ListaDocumentos)
+                {
+                    cantidad++;
+                    resumen += item.ToString() + "\n";
+                }
+            }
 
+            if (cantidad == 0)
+            {
+                resumen = "Nulo";
+            }
         }
 
-        public static void MostrarEnEscaner(Escaner e, out int extencion, out int cantidad, out string resumen)
+        public static void MostrarEnEscaner(Escaner e, out int extension, out int cantidad, out string resumen)
         {
-
+            MostrarDocumentosPorEstado(e, Paso.EnEscaner, out extension, out cantidad, out resumen);
         }
 
-        public static void MostrarEnRevicion(Escaner e, out int extencion, out int cantidad, out string resumen)
+        public static void MostrarEnRevision(Escaner e, out int extension, out int cantidad, out string resumen)
         {
-
+            MostrarDocumentosPorEstado(e, Paso.EnRevision, out extension, out cantidad, out resumen);
         }
 
-        public static void MostrarTerminados(Escaner e, out int extencion, out int cantidad, out string resumen)
+        public static void MostrarTerminados(Escaner e, out int extension, out int cantidad, out string resumen)
         {
-
+            MostrarDocumentosPorEstado(e, Paso.Terminado, out extension, out cantidad, out resumen);
         }
     }
 }
